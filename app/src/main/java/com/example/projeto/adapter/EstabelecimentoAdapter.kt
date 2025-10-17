f0e5ae40be688bf50d8e70f7d409848c907503c3
gisotra <gisotra.henrique@gmail.com>
2025-10-17 09:48:12 -0300

    implementação da base do projeto

---
+++ app/src/main/java/com/example/projeto/adapter/EstabelecimentoAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.projeto.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.projeto.R
+import com.example.projeto.model.Estabelecimento
+
+class EstabelecimentoAdapter(
+    private val context: Context,
+    private val lista: List<Estabelecimento>
+) : ArrayAdapter<Estabelecimento>(context, 0, lista) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val itemView = convertView ?: LayoutInflater.from(context)
+            .inflate(R.layout.itemestabelecimento, parent, false)
+        val contato = lista[position]
+        val imgFoto = itemView.findViewById<ImageView>(R.id.imgFoto)
+        val tvNome = itemView.findViewById<TextView>(R.id.tvNome)
+        val tvTelefone = itemView.findViewById<TextView>(R.id.tvTelefone)
+        val tvEmail = itemView.findViewById<TextView>(R.id.tvEmail)
+
+        return itemView
+    }
+}
---
+++ app/src/main/java/com/example/projeto/model/Estabelecimento.kt
@@ -0,0 +1,9 @@
+package com.example.projeto.model
+
+data class Estabelecimento(
+    val foto: Int,
+    val nome: String,
+    val telefone: String,
+    val email: String,
+    val genero: String
+)
